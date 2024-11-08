0dcb5e25fc18e18d17195b8c593f6b9397c02976
Tarun <tarunsharmakavipurapu@gmail.com>
2024-11-08 13:44:03 +0530

    go -client added

---
+++ go-client/Notes.md
@@ -0,0 +1,4 @@
+### Notes
+
+`go build -ldflags="-H windowsgui" -o screenshot_tool.exe`
+for building as native windows gui
---
+++ go-client/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	// "github.com/go-vgo/robotgo"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/go-vgo/robotgo"
+	hook "github.com/robotn/gohook"
+	"github.com/vcaesar/imgo"
+)
+
+func add() {
+	// fmt.Println("--- Press ` (backtick) to take a screenshot ---")
+
+	// Register backtick key for screenshot
+	hook.Register(hook.KeyDown, []string{"`"}, func(e hook.Event) {
+		// fmt.Println("Backtick pressed - taking screenshot...")
+		takeScreenshot()
+	})
+
+	s := hook.Start()
+	<-hook.Process(s)
+}
+
+func takeScreenshot() {
+
+	bitmap := robotgo.CaptureScreen()
+	defer robotgo.FreeBitmap(bitmap)
+
+	img := robotgo.ToImage(bitmap)
+	basePath := "../server/public/"
+	filename := getnextFileName(basePath)
+	path := filepath.Join(basePath, filename)
+
+	imgo.Save(path, img)
+
+}
+
+func getnextFileName(basePath string) string {
+	counter := 1
+	baseName := "screenshot"
+	ext := ".png"
+
+	for {
+		filename := fmt.Sprintf("%s%d%s", baseName, counter, ext)
+		fullPath := filepath.Join(basePath, filename)
+
+		if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+			// File does not exist, so this filename is available
+			return filename
+		}
+		counter++
+	}
+}
+
+func main() {
+	for {
+		add()
+	}
+}
